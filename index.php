eec4425c85387d92b06fe22bb79342580545c19d
huuthao98 <menofthewind@gmail.com>
2022-05-13 21:42:37 +0700

    add code stupid

---
+++ index.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/fontawesome-free-6.1.1-web/css/all.min.css">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+
+</head>
+<body>
+<header>
+    <nav class="container">
+        <div class="creator">
+            <div class="container">
+                <div class="creator-input">
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-keyboard"></i>
+                        <p class="text">Single input</p>
+                    </div>
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-square-check"></i>
+                        <p class="text">Check box</p>
+                    </div>
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-circle-dot"></i>
+                        <p class="text">Radio group</p>
+                    </div>
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-clipboard-list"></i>
+                        <p class="text">Drop group</p>
+                    </div>
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-message"></i>
+                        <p class="text">Commet group</p>
+                    </div>
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-star"></i>
+                        <p class="text">Rating</p>
+                    </div>
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-ranking-star"></i>
+                        <p class="text">Ranking</p>
+                    </div>
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-images"></i>
+                        <p class="text">Image picker</p>
+                    </div>
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-toggle-off"></i>
+                        <p class="text">Boolen</p>
+                    </div>
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-image"></i>
+                        <p class="text">image</p>
+                    </div>
+                    <div class="input-add-question">
+                        <i class="fa-solid fa-ellipsis"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="" class="logo">Sur<span>vey.</span></a>
+        <div class="links">
+            <ul>
+                <li>
+                    <a href="" class="nav-link">Home</a>
+                </li>
+                <li>
+                    <a href="" class="nav-link">About</a>
+                </li>
+                <li>
+                    <a href="" class="nav-link">Designer</a>
+                </li>
+                <li>
+                    <a href="" class="nav-link">Preview</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <i class="fa-solid fa-eraser icon"></i>
+                </li>
+            </ul>
+            <div class="toggle">
+                <i class="fa-solid fa-moon toggleBtn"></i>
+            </div>
+        </div>
+
+    </nav>
+
+</header>
+<main>
+    <div class="nav-main">
+        <section class="empty-question">
+            <div class="container">
+                <p class="text">The survey is empty. Drag an element from the toolbox or click the button below.</p>
+                <input class="inputPushArray" style="display: none;">
+                <div class="add-question">
+                    Add Question
+                    <i class="fa-solid fa-ellipsis"></i>
+                </div>
+            </div>
+        </section>
+        <section class="show-creator">
+            <!-- <form class="survey-title ">
+                <input  class="survey-title-input" placeholder="Survey Title" type="text" name="" value="">
+                <input class="description-input"  placeholder="Description" type="text">
+            </form>
+
+            <div class="all-pages">
+                <div class="page">
+                    <input  class="page-title-input" placeholder="Page 1" type="text" name="" value="">
+                    <input class="description-input"  placeholder="Description" type="text">
+                    <div class="all-form-input">
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="empty-question">
+                <div class="container">
+                    <div class="add-question">
+                        Add Question
+                        <i class="fa-solid fa-ellipsis icon"></i>
+                    </div>
+                </div>
+            </div> -->
+        </section>
+    </div>
+</main>
+<script src="./js/main.js"></script>
+</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const header =$('header')
-const addQuestion = $(".add-question");
+const addQuestionFirst = $(".add-question");
 const btnEraser = $(".fa-eraser");
 const showCreator = $(".show-creator");
 const emptyQuestion = $(".empty-question");
 
 const btnAddNewQuestion =$(".btnAddNewQuestion")
 const page = $('.page');
-let checkQuestionLocal = localStorage.getItem('hiden');
+
 
 // --------------StickyNavbar----------------
 function stickyNavbar() {
@@ -20,34 +20,107 @@ function stickyNavbar() {
 stickyNavbar();
 
 window.addEventListener("scroll", stickyNavbar);
-// ---------------HIDEN QUESTION ADD----------------
+// ---------------ADD BASIC QUESTION ----------------
+addQuestionFirst.addEventListener("click", ()=>{
+    clickAddItem()
+    emptyQuestion.remove();
+})
 
-//check is question
-checkClassHiden(+checkQuestionLocal)
-function checkClassHiden(isHiden) {
-    if(isHiden) {
-        emptyQuestion.classList.add("hiden")
-        showCreator.classList.remove("hiden")
-    } else {
-        emptyQuestion.classList.remove("hiden")
-        showCreator.classList.add("hiden")
-    }
+
+const arrayQuestion = []
+const inputPushArray = $(".inputPushArray")
+
+function create(action) {
+    
+    arrayQuestion.push({
+        name: action,
+        id: indexId()      
+    })
 }
 
-//add class hiden
-addQuestion.addEventListener("click", ()=> {
-    localStorage.setItem("hiden",1)
-    emptyQuestion.classList.add("hiden")
-    showCreator.classList.remove("hiden")
-});
+function clickAddItem() {
+        create(inputPushArray.value);      
+        render()
+}
 
-btnEraser.addEventListener("click", ()=> {
-    if (confirm('You are going to delete all elements in this survey. Do you want to proceed??')) {
-        emptyQuestion.classList.remove("hiden")
-        showCreator.classList.add("hiden")
-        localStorage.setItem("hiden",0)
+function indexId() {
+    const alphabet = 'abcdefghijklmnouptqxyz1234567890';
+    return alphabet[Math.floor(Math.random()*alphabet.length)] + alphabet[Math.floor(Math.random()*alphabet.length)] 
+    + alphabet[Math.floor(Math.random()*alphabet.length)] + alphabet[Math.floor(Math.random()*alphabet.length)]
+}
+
+function render() {
+   //--------------RENDER ALL PAGE--------------
+    showCreator.innerHTML = `
+    <form class="survey-title ">
+        <input  class="survey-title-input" placeholder="Survey Title" type="text" name="" value="">
+        <input class="description-input"  placeholder="Description" type="text">
+    </form>
+    <div class="all-pages">
+        <div class="page">
+            <input  class="page-title-input" placeholder="Page 1" type="text" name="" value="">
+            <input class="description-input"  placeholder="Description" type="text">
+            <div class="all-form-input">
+                
+            </div>
+        </div>
+    </div>
+    <div class="empty-question">
+        <div class="container">
+            <div class="add-question-continue">
+                Add Question
+                <i class="fa-solid fa-ellipsis icon"></i>
+            </div>
+        </div>
+    </div>`;
+    //--------------RENDER QUESTION FIRST--------------
+    const allFormInput = $(".all-form-input");
+    allFormInput.innerHTML = `
+    <form class="form-input single-input ">
+        <div class="header-question">
+            <div class="number-question">1.</div>
+            <input  class="title-input" placeholder="question" type="text" name="" value="">
+        </div>
+        <div class="input-disable"></div>
+        <div class="impact-question">
+            <div class="impact-question-form impact">
+                Checkbox<i class="fa-solid fa-angle-down"></i>
+            </div>
+            <div class="impact-question-now">
+                <div class="impact"><i class="fa-solid fa-clone"></i>Duplicate</div>
+                <div class="impact"><i class="fa-solid fa-toggle-off"></i>Required</div>
+                <div class="impact"><i class="fa-solid fa-trash-can"></i>Delete</div>
+            </div>
+        </div>
+    </form>`;
+
+    const addQuestionContinue = $(".add-question-continue")
+    addQuestionContinue.addEventListener("click", ()=>{
+        clickAddItem()
+    }) 
+    let question = '';
+    for (let i = 0; i < arrayQuestion.length; i++) {
+        question = question + `
+        <form class="form-input single-input ">
+            <div class="header-question">
+                <div class="number-question">${i+1}.</div>
+                <input  class="title-input" placeholder="question" type="text" name="" value="">
+            </div>
+            <div class="input-disable"></div>
+            <div class="impact-question">
+                <div class="impact-question-form impact">
+                    Checkbox<i class="fa-solid fa-angle-down"></i>
+                </div>
+                <div class="impact-question-now">
+                    <div class="impact"><i class="fa-solid fa-clone"></i>Duplicate</div>
+                    <div class="impact"><i class="fa-solid fa-toggle-off"></i>Required</div>
+                    <div class="impact"><i class="fa-solid fa-trash-can"></i>Delete</div>
+                </div>
+            </div>
+        </form>`;
     }
-
-});
-
-// --------------- ADD NEW QUESION----------------
+    allFormInput.innerHTML = question;
+      
+    // //--------------RENDER QUESTION CONTINUE--------------
+    
+}
